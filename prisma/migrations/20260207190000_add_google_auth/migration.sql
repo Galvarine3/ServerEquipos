b5d20886606efd5e023a2614ef760ebabc713788
Galvarine3 <ariel.wt.93@gmail.com>
2026-02-24 17:49:06 -0300

    Delete prisma directory

--- prisma/migrations/20260207190000_add_google_auth/migration.sql
+++
@@ -1,6 +0,0 @@
--- Add google auth fields
-ALTER TABLE "User"
-  ADD COLUMN "googleSub" TEXT,
-  ALTER COLUMN "passwordHash" DROP NOT NULL;
-
-CREATE UNIQUE INDEX "User_googleSub_key" ON "User"("googleSub");
--- prisma/migrations/20260211200000_add_chat_indexes/migration.sql
+++
@@ -1,6 +0,0 @@
--- Add chat indexes
-CREATE INDEX "CommunityPost_time_idx" ON "CommunityPost"("time" DESC);
-
-CREATE INDEX "Message_postId_fromUserId_time_idx" ON "Message"("postId", "fromUserId", "time");
-CREATE INDEX "Message_postId_time_idx" ON "Message"("postId", "time");
-CREATE INDEX "Message_postId_toUserId_time_idx" ON "Message"("postId", "toUserId", "time");
