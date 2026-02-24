8c16cf4c1c57dc91f81ff23c6300d4ec78c4d27d
Galvarine3 <ariel.wt.93@gmail.com>
2026-02-24 16:21:10 -0300

    Add files via upload

---
+++ prisma/migrations/20260211200000_add_chat_indexes/migration.sql
@@ -0,0 +1,6 @@
+-- Add chat indexes
+CREATE INDEX "CommunityPost_time_idx" ON "CommunityPost"("time" DESC);
+
+CREATE INDEX "Message_postId_fromUserId_time_idx" ON "Message"("postId", "fromUserId", "time");
+CREATE INDEX "Message_postId_time_idx" ON "Message"("postId", "time");
+CREATE INDEX "Message_postId_toUserId_time_idx" ON "Message"("postId", "toUserId", "time");
